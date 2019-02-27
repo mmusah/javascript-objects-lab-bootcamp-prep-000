6213cd76e9b0da9b52aed496015fbc16b4741e70
Muhammad Musah <mmusah1@gmail.com>
2019-02-27 10:35:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,8 @@
+var recipes = {pizza:"cheese"}
+function updateObjectWithKeyAndValue (object,key,value){
+  var fast = {burgerKing:'fries'
+              mcdonalds: 'burger'
+  }
+  return fast
+}
+
