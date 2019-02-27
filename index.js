ad189d46dae7243bbadd0c4cced2f706965f9872
Muhammad Musah <mmusah1@gmail.com>
2019-02-27 10:44:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var recipes = {pizza:"cheese"}
 function updateObjectWithKeyAndValue (object,key,value){
-  var fast = {burgerKing:'fries'}
+  var fast = {prop:'1', 
+              prop2:'2'
+  }
   return fast
 }
 fast.mcdonalds='burgers'
