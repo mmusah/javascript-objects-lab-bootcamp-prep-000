4b5502ec6c2e2ec6b01a55ebea6eb8f968f6c827
Muhammad Musah <mmusah1@gmail.com>
2019-02-27 10:52:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-var recipes = {pizza:"cheese"}
-function updateObjectWithKeyAndValue (object,key,value){
-  var fast = {prop:'1', 
-              prop2:'2'
-  }
-  return fast
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
 }
-function destructivelyUpdateObjectWithKeyAndValue (object,key,value){
-  
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
 }
